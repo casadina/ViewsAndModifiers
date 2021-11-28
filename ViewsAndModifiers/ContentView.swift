028c1bcd80262960bbe15df276d4d2789c0d801e
Casadina <casadina@tuta.io>
2021-11-28 22:40:19 +0800

    Learn order of View modifiers.

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Button("Hello, world!") {
+        }
+        .padding()
+        .background(.red)
+        .padding()
+        .background(.blue)
+        .padding()
+        .background(.green)
+        .padding()
+        .background(.yellow)
     }
 }
 
