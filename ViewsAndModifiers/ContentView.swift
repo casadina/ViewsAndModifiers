7df1398af8b2746b1bb5044756ba1844be81d641
Casadina <casadina@tuta.io>
2021-11-28 22:42:42 +0800

    Learn ternary operator to shorten if statements.

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -8,17 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var useRedText = false
+    
     var body: some View {
         Button("Hello, world!") {
+            // flip the Boolean between true and false
+            useRedText.toggle()
         }
-        .padding()
-        .background(.red)
-        .padding()
-        .background(.blue)
-        .padding()
-        .background(.green)
-        .padding()
-        .background(.yellow)
+        .foregroundColor(useRedText ? .red : .blue)
     }
 }
 
