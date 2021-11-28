36938ee1653fe4c4b7a388e9cee671c5f8fe15e1
Casadina <casadina@tuta.io>
2021-11-28 23:52:51 +0800

    Finish learning about modifying Views through extensions.

--- ViewsAndModifiers/ContentView.swift
+++ ViewsAndModifiers/ContentView.swift
@@ -57,16 +57,30 @@ extension View {
     }
 }
 
+struct GridStack<Content: View>: View {
+    let rows: Int
+    let columns: Int
+    @ViewBuilder let content: (Int, Int) -> Content
+    
+    var body: some View {
+        VStack {
+            ForEach(0..<rows, id: \.self) { row in
+                HStack {
+                    ForEach(0..<columns, id: \.self) {column in
+                        content(row, column)
+                    }
+                }
+            }
+        }
+    }
+}
+
 struct ContentView: View {
     var body: some View {
-        VStack(spacing: 10) {
-            CapsuleText(text: "First")
-                .titleStyle()
-            CapsuleText(text: "Second")
-                .titleStyle()
+        GridStack(rows: 4, columns: 4) {row, col in
+            Image(systemName: "\(row * 4 + col).circle")
+            Text("R\(row) C\(col)")
         }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
-        .watermarked(with: "Yo you gotta buy this stuff.")
     }
 }
 
